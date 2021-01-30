1c646b9712523e33b0594a8e7a1529603a8af87c
aanthonyawad <dev.anthony.awad@gmail.com>
2021-01-30 21:42:00 +0200

    added jenkins project

---
+++ jenkins_project/src/jenkins_project/main.java
@@ -0,0 +1,12 @@
+package jenkins_project;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class main {
+	public static void main(String [] args) {
+		String now = new SimpleDateFormat("dd-mm-yyyy").format(new Date());
+		System.out.print("Hello NSY_205 : "+ now);
+	}
+
+}
