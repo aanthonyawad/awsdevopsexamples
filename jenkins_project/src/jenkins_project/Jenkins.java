2ee3087c649e2b0b2fcb6649e1d6a77694914670
aanthonyawad <dev.anthony.awad@gmail.com>
2021-01-30 22:10:15 +0200

    change in print

--- jenkins_project/src/jenkins_project/Jenkins.java
+++ jenkins_project/src/jenkins_project/Jenkins.java
@@ -3,7 +3,7 @@ package jenkins_project;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class main {
+public class Jenkins {
 	public static void main(String [] args) {
 		String now = new SimpleDateFormat("dd-MM-yyyy : mm-hh").format(new Date());
 		System.out.print("Hello NSY_205 : "+ now);
